36452a0ff5e337efec362b14dac08b0f469b1c44
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-10 18:07:07 +0100

    Add test for an integer and return factorial for the number

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,3 +1,8 @@
 describe Solver do
-  
+  describe '#factorial' do
+    it 'Should takes one argument, an integer and return factorial for the number' do
+      factorial_input = Solver.new.factorial(4)
+      expect(factorial_input).to eq(2)
+    end
+  end
 end
