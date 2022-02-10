8297e7293a569b5d49a2d9295030c130e3788bd0
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-10 18:12:17 +0100

    Add test to check If negative integers, it should raise an exception

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,7 +2,12 @@ describe Solver do
   describe '#factorial' do
     it 'Should takes one argument, an integer and return factorial for the number' do
       factorial_input = Solver.new.factorial(4)
-      expect(factorial_input).to eq(2)
+      expect(factorial_input).to eq(4)
+    end
+
+    it 'If negative integers, it should raise an exception' do
+      negative_integer = Solver.new.factorial(-2)
+      expect(negative_integer).to eq('Exception- Negative number not allowed')
     end
   end
 end
